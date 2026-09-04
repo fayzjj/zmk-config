fa8075e084fa8a0f0b205ba6150c0a14a9d4a5d9
fayzjj <fayzjj@gmail.com>
2026-09-04 13:31:46 +0200

    Add files via upload

--- boards/shields/nice_view_art/custom_status_screen.c
+++ boards/shields/nice_view_art/custom_status_screen.c
@@ -16,18 +16,9 @@
 #include <zmk/events/split_peripheral_status_changed.h>
 #include <zmk/split/bluetooth/peripheral.h>
 
-extern const uint8_t nv_frame_01[1360];
-extern const uint8_t nv_frame_02[1360];
-extern const uint8_t nv_frame_03[1360];
-extern const uint8_t nv_frame_04[1360];
-extern const uint8_t nv_frame_05[1360];
-extern const uint8_t nv_frame_06[1360];
-extern const uint8_t nv_frame_07[1360];
-extern const uint8_t nv_frame_08[1360];
-extern const uint8_t nv_frame_09[1360];
+#include "art_frames.h"
 
-static const uint8_t *const frames[] = { nv_frame_01, nv_frame_02, nv_frame_03, nv_frame_04, nv_frame_05, nv_frame_06, nv_frame_07, nv_frame_08, nv_frame_09 };
-#define FRAME_COUNT 9
+#define FRAME_COUNT NV_FRAME_COUNT
 #define FRAME_PERIOD_MS 600000
 
 static lv_obj_t *img_obj;
@@ -145,7 +136,7 @@ static void draw_status(void) {
 }
 
 static void refresh_frame(void) {
-    memcpy(&frame_map[8], frames[frame_index], 1360);
+    memcpy(&frame_map[8], nv_frames[frame_index], 1360);
     draw_status();
     if (img_obj) {
         lv_img_set_src(img_obj, &frame_dsc);
